30ddc75894a2cccd1e29fc745cd365db19080684
Brian Muller <brian.muller@livingsocial.com>
2011-10-08 11:40:30 -0400

    can now use a file as a message body

--- lib/gridcli/commands/message.rb
+++ lib/gridcli/commands/message.rb
@@ -4,22 +4,30 @@ module GridCLI
   class MessageCommand < BaseCommand
     def initialize
       super "message", "Send a message to a friend or group of friends."
+      add_option("-s subject", "--subject subject", "Subject for message") { |s| @opts[:subject] = s }
+      add_option("-f file", "--file file", "File to use for message body") { |f| @opts[:file] = f }
+      add_option("-b body", "--body body", "Message body") { |b| @opts[:body] = b }
     end
 
     def usage
-      super "<username>[,<username>,...] [<subject>] <body>"
+      super "<username>[,<username>,...]"
     end
 
     def run(args)
       usage if args.length < 2
       recipients = args.shift
-      if args.length > 1 and not args[1].start_with?('-')
-        subject = args.shift
-      else
-        subject = args.first.truncate(30)
-      end
-      body = args.shift
       parse_opts args
+
+      body = nil
+      if not @opts[:file].nil?
+        error("File '#{@opts[:file]}' does not exist.") unless File.exists? @opts[:file]
+        body = File.read(@opts[:file])
+      elsif not @opts[:body].nil?
+        body = @opts[:body]
+      end
+      
+      error("Body cannot be blank. Use one of -f or -b") if body.nil?
+      subject = @opts[:subject].nil? ? body.truncate(30) : @opts[:subject]
 
       begin
         log "Trying to send a message '#{subject}' to '#{recipients}'"
